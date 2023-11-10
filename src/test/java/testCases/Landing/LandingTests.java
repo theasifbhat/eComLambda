49a23c0c59b5131c4eac2f401392714496f75fa9
Asif Bhat <theasifbhat@gmail.com>
2023-11-10 21:13:27 +0530

    completed previous WIP test

--- src/test/java/testCases/Landing/LandingTests.java
+++ src/test/java/testCases/Landing/LandingTests.java
@@ -42,20 +42,21 @@ public void testHeroImages(){
 
 @Test
     public void testPrevButton() throws InterruptedException{
+    List<WebElement> heroItems= landingPage.getHeroItems();
     Actions actions = new Actions(mDriver);
-    for (int i = 0; i < landingPage.getHeroItems().size(); i++) {
+    for (int i = 0; i < heroItems.size(); i++) {
         Thread.sleep(500);
         actions.moveToElement(landingPage.getNextButtonHero()).click().build().perform();
     }
 
-    // need to iterate through the list in reverse order
-    for (WebElement item : landingPage.getHeroItems()){
-        Thread.sleep(500);   // hard wait for 1 second as the class changes temporarily while transitioning
-        if(!item.getAttribute("class").contains("active")){
-            System.out.println("this "+item.getAttribute("class"));
+    int count = heroItems.size()-1;
+    for (WebElement item : heroItems){
+        Thread.sleep(800);   // hard wait for 1 second as the class changes temporarily while transitioning
+        if(!heroItems.get(count).getAttribute("class").contains("active")){
             Assert.fail("Prev button is not working properly");
         }
         actions.moveToElement(landingPage.getPreviousButtonHero()).click().build().perform();
+        count--;
     }
 }
 
